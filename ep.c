c515f441fed565ef0bffd166a5cb13166bbae80c
Alan Moura <54741593+Adamarac@users.noreply.github.com>
2024-11-20 06:33:48 +0000

    Finish project

--- ep.c
+++ ep.c
@@ -5,9 +5,10 @@
  * Lembre-se de substituir o nome do autor, abaixo, pelo seu nome e NUSP.
  * (Redundância com o retorno da função autor())
  * 
- * @author Fábio Nakano NUSP 1048221
+ * @author Alan Moura NUSP 15436668
  */
 #include "ep.h"
+#include "limits.h"
 #if 0 // modelo
 Conjunto Guloso(Conjunto C)
 /∗ C: conjunto de candidatos ∗/
@@ -22,15 +23,19 @@ Conjunto Guloso(Conjunto C)
 }
 #endif
 
+int caminhoPeso = INT_MAX;
 int *grauDoVertice;
-int *arestaUsada;  // flag que informa se uma aresta está sendo usada no caminho que está sendo construído ou não. Ao final, se o algoritmo chegar a uma solução, este array conterá essa solução.
+int *arestaUsada, *arestaSolucao;
+int *visitado;
+int verticeIni;
+ // flag que informa se uma aresta está sendo usada no caminho que está sendo construído ou não. Ao final, se o algoritmo chegar a uma solução, este array conterá essa solução.
 
 /** @brief Esta função é importante para que o corretor automático inclua 
  * corretamente a autoria do EP. Deve retornar Nome e NUSP separados por 
  * ponto-e-virgula, sem pular linhas no meio nem no final.
  */
 char *autor (void) {
-	return "Fábio Nakano ; 1048221";
+	return "Alan Moura ; 15436668";
 }
 
 int ehSolucao (struct grafo *G) { // devido à maneira de construção da solução, verificar que todos os vértices foram alcançados NÃO equivale à existência de um caminho hamiltoniano.
@@ -52,14 +57,14 @@ int ehSolucao (struct grafo *G) { // devido à maneira de construção da soluç
 int ehAceitavel (struct grafo *G, int arestaATestar, int verticeAtual) {
 	if (arestaUsada[arestaATestar]) return 0;   // aresta a testar está em uso. Não é aceitável
 	if (G->A[3*arestaATestar] == verticeAtual) { // arestaATestar conecta no vértice atual
-		if (grauDoVertice[G->A[3*arestaATestar+1]] < 2) { // vértice no outro extremo da aresta não é coberto.
+		//if (grauDoVertice[G->A[3*arestaATestar+1]] < 2) { // vértice no outro extremo da aresta não é coberto.
 			return 1; // é aceitável
-		}
+		//}
 	}
 	if (G->A[3*arestaATestar+1] == verticeAtual) { // arestaATestar conecta no vértice atual.
-		if (grauDoVertice[G->A[3*arestaATestar]] < 2) { // vértice no outro extremo da aresta não é coberto.
+		//if (grauDoVertice[G->A[3*arestaATestar]] < 2) { // vértice no outro extremo da aresta não é coberto.
 			return 1; // é aceitável
-		}
+		//}
 	}
 	return 0;
 }
@@ -74,6 +79,78 @@ int aumentaCaminho (struct grafo *G, int arestaAcrescentar, int verticeAtual) { 
 	}
 	return G->A[3*arestaAcrescentar];
 }
+
+void diminuiCaminho (struct grafo *G, int arestaAcrescentar) {  
+	arestaUsada[arestaAcrescentar]=0; 
+	grauDoVertice[G->A[3*arestaAcrescentar+1]]--;
+	grauDoVertice[G->A[3*arestaAcrescentar]]--;
+}
+
+
+int backtracking(struct grafo *G, int nivel, int verticeAtual) {
+
+	//printf("Nova chamada");
+	//printf(" Vertice atual: %d ", verticeAtual);
+	//printf(" Nivel: %d \n", nivel);
+
+    if (nivel == G->N) {
+        for (int j = 0; j < G->M; j++) {
+            if ((G->A[3 * j] == verticeAtual && G->A[3 * j + 1] == verticeIni) ||
+                (G->A[3 * j + 1] == verticeAtual && G->A[3 * j] == verticeIni)) {
+				// Calcula peso do caminho
+
+				arestaUsada[j] = 1;
+
+                int caminhoteste = 0;
+                for (int k = 0; k < G->M; k++) {
+                    if (arestaUsada[k] == 1) caminhoteste += G->A[3 * k + 2];
+                }
+
+                if (caminhoteste < caminhoPeso){ caminhoPeso = caminhoteste;
+					//printf("\n");
+					for (int k = 0; k < G->M; k++) {
+						arestaSolucao[k] = arestaUsada[k]; 
+						//printf(" %d ", arestaSolucao[k]);
+					}
+					//printf("\n");
+				}else{
+					arestaUsada[j] = 0;
+				}
+
+                return 1;
+            }
+        }
+        return 0; // Não há ciclo válido
+    }
+
+    int encontrouSolucao = 0;
+    for (int i = 0; i < G->M; i++) {
+        int proximoVertice = -1;
+
+        if (G->A[3 * i] == verticeAtual && !visitado[G->A[3 * i + 1]]) {
+            proximoVertice = G->A[3 * i + 1];
+        } else if (G->A[3 * i + 1] == verticeAtual && !visitado[G->A[3 * i]]) {
+            proximoVertice = G->A[3 * i];
+        }
+
+        if (proximoVertice != -1 && ehAceitavel(G, i, verticeAtual)) {
+
+			//printf("Aresta usada: %d \n", i);
+
+            aumentaCaminho(G, i, verticeAtual);       // Marca aresta como usada
+            visitado[proximoVertice] = 1;             // Marca o vértice como visitado
+            encontrouSolucao |= backtracking(G, nivel + 1, proximoVertice);
+			//diminuiCaminho(G,i);
+			//printf("Aresta desusada: %d \n", i);
+			//printf("Vertice desvisitado: %d \n", proximoVertice);
+            visitado[proximoVertice] = 0;             // Desmarca o vértice
+            arestaUsada[i] = 0;                       // Desmarca a aresta
+        }
+    }
+
+    return encontrouSolucao;
+}
+
 
 int guloso(struct grafo *G, int verticeAtual ) {
   /* inicializa seleção de movimentos; */
@@ -97,12 +174,16 @@ int guloso(struct grafo *G, int verticeAtual ) {
 
 int iniciaEexecuta(struct grafo *G, int verticeInicial) {
     grauDoVertice=calloc(G->N, sizeof(int)); // necessário inicializar com zeros;
-    arestaUsada=calloc(G->M, sizeof(int));    // necessário inicializar com zeros;
+    arestaUsada=calloc(G->M, sizeof(int));
+	arestaSolucao=calloc(G->M, sizeof(int)); // necessário inicializar com zeros;
+	verticeIni = verticeInicial;
+	visitado = calloc(G->N, sizeof(int));
 // como o circuito Hamiltoniano inclui todos os vértices do grafo, tanto faz por onde começa. 	
-    int r=guloso(G,verticeInicial);
+	visitado[verticeIni] = 1;
+    int r=backtracking(G, 1, verticeIni);
 	for (int i=0;i<G->M;i++) {  /**< ATENÇÃO: TEM QUE TER ESTA PARTE PARA A RESPOSTA PODER SER CORRIGIDA!! */
 	// como arestausada não é restaurada no retorno da recursão, quando acha o circuito, posso passar para o imprimeMermaid.
-		G->S[i]=arestaUsada[i];
+		G->S[i]=arestaSolucao[i];
 	}
 	return r;
 }
